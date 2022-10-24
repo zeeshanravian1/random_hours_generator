280f5fbf337f5c19feefa82c20bc89c955dfc5cb
zeeshanravian1 <62133352+zeeshanravian1@users.noreply.github.com>
2022-10-25 00:43:31 +0500

    Update random_hours_generator.py

--- random_hours_generator.py
+++ random_hours_generator.py
@@ -3,9 +3,13 @@ from random import randrange
 
 
 # Generate the random hour and append in dataframe
-data = pd.read_excel("df2.xlsx")
+data = pd.read_excel("file_name.xlsx")
 
 # Create a dictionary and assign values to column 
 d1 = {'Time' : pd.date_range(start = '10-10-2022', end = '11-10-2022', freq = f'{randrange(0,9)}H')}
 d2 = data.assign(DateAndTimeRecent = pd.DataFrame(d1))
 print(d2)
+
+# Write data in excel file
+with pd.ExcelWriter("filename.xlsx") as writer:
+    d2.to_excel(writer)
